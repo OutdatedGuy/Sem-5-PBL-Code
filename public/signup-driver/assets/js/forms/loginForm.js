02c96a84c842b9fd4f8fa03a64b69c562cd2e556
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-11-22 03:53:21 +0530

    Added login form setter

---
+++ public/signup-driver/assets/js/forms/loginForm.js
@@ -0,0 +1,38 @@
+import { registerDriver } from "../api/registerDriver.js";
+
+let loginInfo;
+
+import { prevStep } from "./progressBar.js";
+
+const setLoginForm = () => {
+  const loginFormContainer = document.querySelector("#login-form");
+  const loginForm = document.querySelector("#loginForm");
+  const prevBtn = loginForm.querySelector(".prev-btn");
+
+  prevBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    loginFormContainer.style.display = "none";
+    document.querySelector("#license-form").style.display = "block";
+
+    prevStep();
+  });
+
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    if (loginForm.password.value !== loginForm.confirmPassword.value) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    loginInfo = {
+      userName: loginForm.userName.value,
+      password: loginForm.password.value,
+    };
+
+    await registerDriver();
+  });
+};
+
+export { setLoginForm, loginInfo };
