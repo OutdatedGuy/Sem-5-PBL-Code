1cb9a4360b49610fa831b1258af092dc9695bc02
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-11-22 03:11:25 +0530

    Added a login form setter

---
+++ public/signup-user/assets/js/forms/loginForm.js
@@ -0,0 +1,38 @@
+import { registerUser } from "../api/registerUser.js";
+
+let loginInfo;
+
+import { prevStep } from "./progressBar.js";
+
+const setLoginForm = () => {
+  const loginFormContainer = document.querySelector("#login-form");
+  const loginForm = document.querySelector("#loginForm");
+  const prevBtn = loginForm.querySelector(".prev-btn");
+
+  prevBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    loginFormContainer.style.display = "none";
+    document.querySelector("#user-form").style.display = "block";
+
+    prevStep();
+  });
+
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    if (loginForm.password.value !== loginForm.confirmPassword.value) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    loginInfo = {
+      userName: loginForm.userName.value,
+      password: loginForm.password.value,
+    };
+
+    await registerUser();
+  });
+};
+
+export { setLoginForm, loginInfo };
