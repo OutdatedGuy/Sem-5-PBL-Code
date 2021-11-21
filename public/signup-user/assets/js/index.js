ed49a8e4ec8c91d104b2822b4bc9fb826f4ec890
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-11-22 03:13:15 +0530

    Called necessary function in after loading

--- public/signup-user/assets/js/index.js
+++ public/signup-user/assets/js/index.js
@@ -1,34 +1,7 @@
-const form = document.querySelector("form");
-const submitBtn = document.querySelector("#submitBtn");
+import { setUserForm } from "./forms/userForm.js";
+import { setLoginForm } from "./forms/loginForm.js";
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  if (form.password.value !== form.confirmPassword.value) {
-    alert("Passwords do not match");
-    return;
-  }
-
-  const data = {
-    fullName: form.fullName.value,
-    gender: form.gender.value,
-    birthDate: form.birthDate.value,
-    email: form.email.value,
-    phone: form.phone.value,
-    userName: form.userName.value,
-    password: form.password.value,
-  };
-
-  const arg = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  };
-
-  const response = await fetch("/api/userRegistration", arg);
-  const result = await response.json();
-
-  console.log(result);
+document.addEventListener("DOMContentLoaded", () => {
+  setUserForm();
+  setLoginForm();
 });
