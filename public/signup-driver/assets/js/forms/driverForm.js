e9ad83fcc4105ada21a087d6bef81cda739f9bbf
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-11-23 00:51:11 +0530

    Added max birthDate limit

--- public/signup-driver/assets/js/forms/driverForm.js
+++ public/signup-driver/assets/js/forms/driverForm.js
@@ -6,6 +6,16 @@ const setDriverForm = () => {
   const driverFormContainer = document.querySelector("#driver-form");
   const driverForm = document.querySelector("#driverForm");
 
+  // Set birthDate form max date to today
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0");
+  const yyyy = today.getFullYear();
+  const todayDate = yyyy + "-" + mm + "-" + dd;
+  driverForm
+    .querySelector("input[name='birthDate']")
+    .setAttribute("max", todayDate);
+
   driverForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
--- public/signup-user/assets/js/forms/userForm.js
+++ public/signup-user/assets/js/forms/userForm.js
@@ -6,6 +6,16 @@ const setUserForm = () => {
   const userFormContainer = document.querySelector("#user-form");
   const userForm = document.querySelector("#userForm");
 
+  // Set birthDate form max date to today
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0");
+  const yyyy = today.getFullYear();
+  const todayDate = yyyy + "-" + mm + "-" + dd;
+  userForm
+    .querySelector("input[name='birthDate']")
+    .setAttribute("max", todayDate);
+
   userForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
