cae64a8e6c41fcdd934c7a633651e73bbd29f60e
Vinayak Mali <malivinayak@gmail.com>
2021-11-23 00:12:17 +0530

    Connected to main database

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
-  database: 'test',
+  database: 'Taxi_management_system',
   insecureAuth: true
 });
 
@@ -13,16 +13,6 @@ connection.connect(function (err) {
   console.log("Connected!");
 });
 
-// let query = "insert into user values('Vinayak Mali','Male')";
-// connection.query(query, function (error, results, fields) {
-//   if (error)
-//       throw error;
 
-//   results.forEach(result => {
-//       console.log(result);
-//   });
-// });
-
-// connection.end();
 export { connection };
-//insert into user(birth,date) values()
+
