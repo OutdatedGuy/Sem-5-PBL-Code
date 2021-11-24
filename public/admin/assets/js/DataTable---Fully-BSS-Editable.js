4dd2c7212fea851e636c5b475284471f89ba7f1d
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-11-24 19:16:16 +0530

    Added admin-page structure

---
+++ public/admin/assets/js/DataTable---Fully-BSS-Editable.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.mydatatable').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ],
+        scrollY: 700,
+        scrollX: true,
+        autoWidth: false,
+        scrollCollapse: true,
+        paging: true
+    });
+});
