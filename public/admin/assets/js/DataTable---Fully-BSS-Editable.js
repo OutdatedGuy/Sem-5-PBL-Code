12f55c43fd78ffa417fe827e0139e580c3ca7f2e
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-11-24 21:12:18 +0530

    Converted to a function

--- public/admin/assets/js/DataTable---Fully-BSS-Editable.js
+++ public/admin/assets/js/DataTable---Fully-BSS-Editable.js
@@ -1,13 +1,14 @@
-$(document).ready(function() {
-    $('.mydatatable').DataTable({
-        dom: 'Bfrtip',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ],
-        scrollY: 700,
-        scrollX: true,
-        autoWidth: false,
-        scrollCollapse: true,
-        paging: true
-    });
-});
+const setTable = function () {
+  $(".mydatatable").DataTable({
+    dom: "Bfrtip",
+    buttons: ["copy", "csv", "excel", "pdf", "print"],
+    scrollY: 700,
+    scrollX: true,
+    autoWidth: false,
+    scrollCollapse: true,
+    paging: true,
+    destroy: true,
+  });
+};
+
+export { setTable };
