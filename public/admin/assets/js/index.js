80ddf0311f31fa25c0cfcf1579e4174d9cc1c788
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-11-24 21:13:01 +0530

    Used to table setter function

--- public/admin/assets/js/index.js
+++ public/admin/assets/js/index.js
@@ -2,10 +2,12 @@ import { setDriverQueryForm } from "./forms/setDriverQueryForm.js";
 import { setUserQueryForm } from "./forms/setUserQueryForm.js";
 import { setTripQueryForm } from "./forms/setTripQueryForm.js";
 import { setCustomQueryForm } from "./forms/setCustomQueryForm.js";
+import { setTable } from "./DataTable---Fully-BSS-Editable.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+$(document).ready(() => {
   setDriverQueryForm();
   setUserQueryForm();
   setTripQueryForm();
   setCustomQueryForm();
+  setTable();
 });
