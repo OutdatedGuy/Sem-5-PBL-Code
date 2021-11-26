102929e3f33fa5fa417b8e620c2d3a5f87009379
Vinayak Mali <malivinayak@gmail.com>
2021-11-26 15:48:39 +0530

    Added driver query api for admin page

---
+++ routes/driverQuery.js
@@ -0,0 +1,62 @@
+import { connection } from "../database.js";
+
+const driverQuery = (req, res) => {
+  console.log(req.body);
+
+  try {
+    const { fullName, gender, birthDateR1, birthDateR2, ageR1, ageR2, city, pincode } =
+      req.body;
+
+    let query = `select name, gender, birth, age, mail, phone, city, pincode from driver where `;
+    query += fullName ? `name like "%${fullName}%" and ` : "";
+    query += gender ? `gender="${gender}" and ` : "";
+    query += birthDateR1 ? `birth>="${birthDateR1}" and ` : "";
+    query += birthDateR2 ? `birth<="${birthDateR2}" and ` : "";
+    query += ageR1 ? `age>="${Math.floor(ageR1)}" and ` : "";
+    query += ageR2 ? `age<="${Math.floor(ageR2)}" and ` : "";
+    query += city ? `city="${city}" and ` : "";
+    query += pincode ? `pincode="${pincode}" and ` : "";
+
+    if (query.endsWith("and "))
+      query = query.substr(0, query.length - 4);
+    else if (query.endsWith("where "))
+      query = query.substr(0, query.length - 6);
+
+    console.log(query);
+
+    connection.query(query, function (error, results) {
+      if (error) throw error;
+      // console.log({results});
+      if (results.length == 0) {
+        return res.send({
+          status: "success",
+          code: 200,
+          data: {
+            properties: [],
+            values: [],
+          }
+        });
+      } else {
+        return res.send({
+          status: "success",
+          code: 200,
+          data: {
+            properties: Object.getOwnPropertyNames(results[0]),
+            values: results,
+          }
+        });
+      }
+    });
+  }
+  catch (err) {
+    console.log(err);
+    return res.status(500).send({
+      message: "User Registration Failed!!!",
+      status: "failure",
+      code: 500,
+    });
+  }
+
+};
+
+export { driverQuery };
