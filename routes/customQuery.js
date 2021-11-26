30dcf7a257b3d375e3143f2f7225909cf229e2da
Vinayak Mali <malivinayak@gmail.com>
2021-11-26 15:50:30 +0530

    Added custom query api for admin page

---
+++ routes/customQuery.js
@@ -0,0 +1,45 @@
+import { connection } from "../database.js";
+
+const customQuery = (req, res) => {
+  console.log(req.body);
+
+  try {
+    const { customQuery } = req.body;
+
+    console.log(customQuery);
+
+    connection.query(customQuery, function (error, results) {
+      if (error) throw error;
+      // console.log({results});
+      if (results.length == 0) {
+        return res.send({
+          status: "success",
+          code: 200,
+          data: {
+            properties: [],
+            values: [],
+          }
+        });
+      } else {
+        return res.send({
+          status: "success",
+          code: 200,
+          data: {
+            properties: Object.getOwnPropertyNames(results[0]),
+            values: results,
+          }
+        });
+      }
+    });
+  }
+  catch (err) {
+    console.log(err);
+    return res.status(500).send({
+      message: "Something went Wrong!!!",
+      status: "failure",
+      code: 500,
+    });
+  }
+};
+
+export { customQuery };
