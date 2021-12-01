f224305f3de0bc5b6dcbd409cd5ba6ff2e1740ee
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-12-01 15:32:00 +0530

    Added credential check logic

---
+++ public/admin/assets/js/api/adminInfo.js
@@ -0,0 +1,34 @@
+(async () => {
+  document.querySelector("html").style.display = "none";
+  // get params from url
+  const urlParams = new URLSearchParams(window.location.search);
+  const token = urlParams.get("token");
+
+  if (!token) {
+    window.location.replace("../admin-login/");
+  } else {
+    // get admin info
+    try {
+      const response = await fetch("/api/credentials/admin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: token,
+        }),
+      });
+      const result = await response.json();
+      if (result.code === 200) {
+        document.querySelector(".admin-name").innerText +=
+          result.data.adminName;
+      } else {
+        window.location.replace("../admin-login/");
+      }
+    } catch (error) {
+      console.log(error);
+      window.location.replace("../admin-login/");
+    }
+  }
+  document.querySelector("html").style.display = "block";
+})();
