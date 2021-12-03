40579f8c0987dade59e449a8b8df5af74a0f2541
Vinayak Mali <malivinayak@gmail.com>
2021-12-04 02:13:16 +0530

    Best Modified accept trip api by VINAYAK

--- routes/trip/acceptingTrip.js
+++ routes/trip/acceptingTrip.js
@@ -4,10 +4,7 @@ const acceptingTrip = (req, res) => {
   // console.log(req.body);
 
   try {
-    const {
-      tripID,
-      token
-    } = req.body
+    const { tripID, token } = req.body
 
     if (!tripID || !token) {
       console.log("Not all fields provided!!!");
@@ -30,47 +27,92 @@ const acceptingTrip = (req, res) => {
           message: "Invalid token",
         });
       } else {
+        const tripIdCheck = `select trip_id from trip where trip_id = ${tripID}`;
 
-        const statusQuery = `select status, fare from trip where trip_id=${tripID}`;
-
-        connection.query(statusQuery, function (error, results2) {
+        //Query to get valid Trip ID if user enter ngatve or tripid which is not exist in trip table
+        connection.query(tripIdCheck, function (error, results2) {
           if (error) throw new Error(error);
-          console.log(results2);
 
-          const status = results2[0].status;
-          if (status === 0) {
-
-            const insertQuery = `update trip set driver_id = ${results1[0].driver_id} and status = 1 where trip_id=${tripID}`;
-            connection.query(insertQuery, function (error, results3) {
-              if (error) throw new Error(error);
-
-              const newEarning = results1[0].t_earn + results2[0].fare;
-              const updateQuery = `update driver set t_earn = ${newEarning} where driver_id=${results1[0].     driver_id}`;
-              connection.query(updateQuery, function (error) {
-                if (error) throw new Error(error);
-                connection.query(statusQuery, function (error, results2) {
-                  return res.send({
-                    message: "Trip Accepted Successful...",
-                    status: "success",
-                    code: 200,
-                  });
-                });
-              });
-            });
-          }
-          else {
+          if (results2.length === 0) {
             return res.send({
-              message: "This trip is already booked...",
               status: "failure",
               code: 400,
+              message: "Entered Trip ID is not valid...\nPlease Provide Valid Trip ID to continue.",
             });
-          }
+          } else {
+            const tripid = results2[0].trip_id;
 
-          return res.send({
-            message: "Trip Booked Successful...",
-            status: "success",
-            code: 200,
-          });
+            if (tripid == tripID) {                                           //Check for Valid Trip ID
+              const statusQuery = `select trip_id, status, fare, ac from trip where trip_id = ${tripID}`;
+
+              connection.query(statusQuery, function (error, results3) {        //Query to get current status of Trip
+                if (error) throw new Error(error);
+                console.log(results3);
+
+                const taxitypeQuery = `select ac from taxi where driver_id = ${results1[0].driver_id}`;
+                connection.query(taxitypeQuery, function (error, results4) {      //Query for getting taxi type 
+                  if (error) throw new Error(error);
+                  const taxiAc = results4[0].ac;
+
+                  const status = results3[0].status;
+                  const ac = results3[0].ac;
+
+                  if (ac === taxiAc) {    //Checking for driver entering valid trip ID for taxi Type
+                    if (status === 0) {   //Checking Status of Trip
+                      const insertdriverQuery = `update trip set driver_id = ${results1[0].driver_id} where trip_id = ${tripID}`;
+
+                      connection.query(insertdriverQuery, function (error) {      //Assigning Driver to Trip
+                        if (error) throw new Error(error);
+
+                        const updatestatusQuery = `update trip set status = 1 where trip_id = ${tripID}`;
+                        connection.query(updatestatusQuery, function (error) {    //Updating Trip status toCompleted
+                          if (error) throw new Error(error);
+
+                          const newEarning = results1[0].t_earn + results3[0].fare;
+                          const updateQuery = `update driver set t_earn = ${newEarning} where driver_id = ${results1[0].driver_id}`;
+                          connection.query(updateQuery, function (error) {
+                            if (error) throw new Error(error);
+
+                            connection.query(statusQuery, function (error) {
+                              if (error) throw new Error(error);
+
+                              return res.send({
+                                message: "Trip Accepted Successful...",
+                                status: "success",
+                                code: 200,
+                              });
+                            });
+                          });
+                        });
+                      });
+                    }
+                    else {
+                      return res.send({
+                        message: "This trip is already booked...\nLet's go with another one",
+                        status: "failure",
+                        code: 400,
+                      });
+                    }
+                  }
+                  else {
+                    return res.send({
+                      message: "Check Entered Trip ID...\nThis trip don't match with your taxi type\nLet's go with another one from following list",
+                      status: "failure",
+                      code: 400,
+                    });
+                  }
+
+                });
+              });
+            }
+            else {
+              return res.send({
+                message: "Invalid Trip ID...",
+                status: "failure",
+                code: 400,
+              });
+            }
+          }
         });
       }
     });
