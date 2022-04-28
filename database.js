ed0c0fec9d7cb6e6f84b73718b9a7726b18168e0
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2022-04-28 21:34:22 +0530

    refactor: increasing connection timeout

--- database.js
+++ database.js
@@ -8,6 +8,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectTimeout: 1800000,
   insecureAuth: true,
 });
 
