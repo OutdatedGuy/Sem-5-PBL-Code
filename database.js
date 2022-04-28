eb0ba595e6c21f92633f73c3849f79d7c47e460c
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2022-04-28 20:37:14 +0530

    feat: using dotenv to acces private data

--- database.js
+++ database.js
@@ -1,10 +1,12 @@
 import mysql from "mysql";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "Taxi_management_system",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   insecureAuth: true,
 });
 
